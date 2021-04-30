ce9c34e21b3a4dc9d760eddfdbf204a5cb78c06b
Yuri Yakovlev <hi@mynameisyuri.com>
2021-04-30 14:17:34 +0200

    Bump the deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  mode: 'jit',
   // darkMode: 'media', // 'media' or 'class'
   theme: {
     extend: {
