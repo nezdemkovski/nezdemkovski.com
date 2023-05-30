9fc382627144baf32eac3556910b5f2d0c05ec17
Yuri Nezdemkovski <yuri@nezdemkovski.com>
2023-05-30 14:43:33 +0200

    feat: add recommendations page

---
+++ app/contact/recommendations/page.tsx
@@ -0,0 +1,11 @@
+import Info from './info.mdx';
+
+const RecommendationsPage = async () => {
+  return (
+    <main className="prose prose-lg m-6 mx-auto max-w-3xl">
+      <Info />
+    </main>
+  );
+};
+
+export default RecommendationsPage;
